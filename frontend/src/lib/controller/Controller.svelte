06c461132b75d80d6efdfcd99c45ee4e4fe7a5e6
ReidS28 <strawj3434@gmail.com>
2026-02-06 20:41:21 -0500

    2 useless buttons

---
+++ frontend/src/lib/controller/Controller.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	interface Props {
+		controllerID: number;
+	}
+	let { controllerID = 0 }: Props = $props();
+</script>
+
+<button onclick={() => controllerID++}>
+    controllerID is {controllerID}
+</button>
